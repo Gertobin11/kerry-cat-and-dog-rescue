74268009f3b3ea723b53544f97a07baa4bf1bb2a
Gertobin11 <gertobin11@gmail.com>
2022-03-08 14:48:54 +0000

    added style and javascript to the hamburger menu for smaller screen sizes, added footer content and styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,41 @@
-let catsLink = document.getElementById("cats-link")
-let catList = document.getElementById("cats-list")
-let dogsLink = document.getElementById("dogs-link")
-let dogList = document.getElementById("dogs-list")
-let logoDiv = document.querySelector(".logo-div")
-let logo = document.querySelector(".logo")
-let downIcons = Array.from(document.getElementsByClassName("fa"))
-let dropDownItems = Array.from(document.querySelectorAll(".drop-list"))
+let catsLink = document.getElementById("cats-link");
+
+let catList = document.getElementById("cats-list");
+
+let dogsLink = document.getElementById("dogs-link");
+
+let dogList = document.getElementById("dogs-list");
+
+let logoDiv = document.querySelector(".logo-div");
+
+let logo = document.querySelector(".logo");
+
+let downIcons = Array.from(document.getElementsByClassName("fa"));
+
+let dropDownItems = Array.from(document.querySelectorAll(".drop-list"));
+
+let menuBtn = document.querySelector(".menu-btn");
+
+let navList = document.querySelector(".nav-list")
+
+let menuOpen = false;
+
+let catsLinkOpen = false;
+
+let dogsLinkOpen = false;
+
+menuBtn.addEventListener("click", () => {
+    if(!menuOpen) {
+        menuBtn.classList.add("open")
+        navList.classList.add("show", "show-ul")
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("open", "show-ul")
+        navList.classList.remove("show")
+        menuOpen = false;
+    }
+})
+
 catList.classList.add("hide");
 dogList.classList.add("hide");
 
@@ -29,7 +59,6 @@ hideDropDownItems()
 
 
 catsLink.addEventListener("mouseleave", function (event) {
-    console.log('listeneradded')
     catList.classList.remove("show")
     catList.classList.add("hide")
     hideDropDownItems()
@@ -41,7 +70,6 @@ catsLink.addEventListener("mouseenter", function (event) {
 })
 
 dogsLink.addEventListener("mouseleave", function (event) {
-    console.log('listeneradded')
     dogList.classList.remove("show")
     dogList.classList.add("hide")
     hideDropDownItems()
@@ -49,7 +77,6 @@ dogsLink.addEventListener("mouseleave", function (event) {
 
 dogsLink.addEventListener("mouseenter", function (event) {
     dogList.classList.add("show", "show-ul")
-    console.log('show')
     setTimeout(showDropDownItems, 200)
 })
 
@@ -63,16 +90,44 @@ logoDiv.addEventListener("mouseleave", function (event) {
 
 catsLink.addEventListener("mouseenter", function (event) {
     downIcons[0].classList.add("drop")
+    catsLinkOpen = true;
 })
 
 catsLink.addEventListener("mouseleave", function (event) {
     downIcons[0].classList.remove("drop")
+    catsLinkOpen = false;
 })
 
 dogsLink.addEventListener("mouseenter", function (event) {
     downIcons[1].classList.add("drop")
+    dogsLinkOpen = true;
 })
 
 dogsLink.addEventListener("mouseleave", function (event) {
     downIcons[1].classList.remove("drop")
+    dogsLinkOpen = true
+})
+
+dogsLink.addEventListener("click", () => {
+    if (dogsLinkOpen){
+        dogList.classList.remove("show")
+        dogList.classList.add("hide")
+        dogsLinkOpen = false;
+    } else {
+        dogList.classList.add("show")
+        dogList.classList.remove("hide")
+        dogsLinkOpen = true;
+    }
+})
+
+catsLink.addEventListener("click", () => {
+    if (catsLinkOpen) {
+        catList.classList.remove("show")
+        catList.classList.add("hide")
+        catsLinkOpen = false;
+    } else {
+        catList.classList.add("show")
+        catList.classList.remove("hide")
+        catsLinkOpen = true;
+    }
 })
