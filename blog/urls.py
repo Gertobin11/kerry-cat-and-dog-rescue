c46dcea4a568ad9ee655664cdcf81ccfb95ff905
Gertobin11 <gertobin11@gmail.com>
2022-03-08 16:16:18 +0000

    added post-detail view, template, url and style

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="blog")
+    path("", views.PostList.as_view(), name="blog"),
+    path("<slug>", views.PostDetail.as_view(), name="blog_details")
 ]
