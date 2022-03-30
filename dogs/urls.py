0016d21d5786d78022c286d47bfd512b63df5bbf
Gertobin11 <gertobin11@gmail.com>
2022-03-30 15:01:01 +0000

    added the dog list html and the dog neuter html to the project along with the urls and views

--- dogs/urls.py
+++ dogs/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("dog-adoption", views.DogAdoptionView.as_view(), name="dog-adoption")
+    path("dog-adoption", views.DogAdoptionView.as_view(), name="dog-adoption"),
+    path("dog-list", views.DogView.as_view(), name="dog-list"),
+    path("dog-neuter", views.dog_neuter_info, name="dog-neuter")
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 from django.views.generic.edit import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
+from blog.models import Post
 from .models import DogAdoption
 from .forms import DogAdoptionForm
 
@@ -13,3 +15,18 @@ class DogAdoptionView(SuccessMessageMixin, CreateView):
     template_name = 'dog-adoption.html'
     success_message = ('Your Application made through %(email)s was'
                        ' uploaded successfully')
+
+
+class DogView(generic.ListView):
+    """ A view to display al the posts associated with dogs """
+    model = Post
+    queryset = Post.objects.filter(category=2).order_by('-created_on')
+    paginate_by = 6
+    template_name = 'dog-list.html'
+    
+
+def dog_neuter_info(request):
+    """ Function to display information about neutering """
+    template = 'dog-neuter.html'
+
+    return render(request, template)
